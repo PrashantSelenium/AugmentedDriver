55f6e62ee3f5b74b88ee6660fd25e3cfe7974d5f
Gabriel Such <gabrielsuch@gmail.com>
2016-02-03 00:22:31 -0800

    Remove a lot of warnings of compilers/eclipse.

--- src/main/java/com/salesforceiq/augmenteddriver/runners/TestRunner.java
+++ src/main/java/com/salesforceiq/augmenteddriver/runners/TestRunner.java
@@ -1,39 +1,35 @@
 package com.salesforceiq.augmenteddriver.runners;
 
-import com.google.common.base.Preconditions;
-import com.google.inject.Inject;
-import com.google.inject.assistedinject.Assisted;
-import com.salesforceiq.augmenteddriver.integrations.IntegrationManager;
-import com.salesforceiq.augmenteddriver.util.Util;
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Request;
 import org.junit.runner.Result;
 
-import java.io.ByteArrayOutputStream;
-import java.lang.reflect.Method;
-import java.util.concurrent.Callable;
+import com.google.common.base.Preconditions;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.salesforceiq.augmenteddriver.integrations.IntegrationManager;
+import com.salesforceiq.augmenteddriver.util.Util;
 
 /**
  * Knows how to run one test.
  */
 public class TestRunner implements Callable<AugmentedResult> {
-    private static final Log LOG = LogFactory.getLog(TestRunner.class);
+    private static final Log logger = LogFactory.getLog(TestRunner.class);
 
     private final Method test;
-    private final ByteArrayOutputStream outputStream;
-    private final String nameAppender;
+    private final ByteArrayOutputStream output;
     private final IntegrationManager integrationManager;
 
     @Inject
-    public TestRunner(@Assisted Method test,
-                      @Assisted String nameAppender,
-                      ByteArrayOutputStream outputStream,
-                      IntegrationManager integrationManager) {
+    public TestRunner(@Assisted Method test, ByteArrayOutputStream outputStream, IntegrationManager integrationManager) {
         this.test = Preconditions.checkNotNull(test);
-        this.nameAppender = Preconditions.checkNotNull(nameAppender);
-        this.outputStream = Preconditions.checkNotNull(outputStream);
+        this.output = Preconditions.checkNotNull(outputStream);
         this.integrationManager = Preconditions.checkNotNull(integrationManager);
     }
 
@@ -46,12 +42,12 @@ public class TestRunner implements Callable<AugmentedResult> {
         long start = System.currentTimeMillis();
 
         try {
-            LOG.info(String.format("STARTING Test %s", testName));
+            logger.info(String.format("STARTING Test %s", testName));
             Result result = jUnitCore.run(Request.method(test.getDeclaringClass(), test.getName()));
-            LOG.info(String.format("FINISHED Test %s in %s", testName, Util.TO_PRETTY_FORMAT.apply(System.currentTimeMillis() - start)));
-            return new AugmentedResult(result, outputStream);
+            logger.info(String.format("FINISHED Test %s in %s", testName, Util.TO_PRETTY_FORMAT.apply(System.currentTimeMillis() - start)));
+            return new AugmentedResult(result, output);
         } finally {
-            outputStream.close();
+            output.close();
         }
     }
 
@@ -60,9 +56,9 @@ public class TestRunner implements Callable<AugmentedResult> {
 
         integrationManager
                 .enabledReportIntegrations()
-                .forEach(integration -> jUnitCore.addListener(integration.getReporter(outputStream, nameAppender)));
+                .forEach(integration -> jUnitCore.addListener(integration.getReporter(output)));
 
         return jUnitCore;
     }
-
+    
 }
